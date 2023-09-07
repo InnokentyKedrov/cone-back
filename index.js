e85c3ca5a8d72395bcb7481a51640c4ad9603850
Andrey <kedr_club@mail.ru>
2023-09-07 10:51:42 +0300

    fix: cors work

--- index.js
+++ index.js
@@ -2,14 +2,53 @@ const express = require("express");
 const bodyparser = require("body-parser");
 
 const PORT = process.env.PORT || 3010;
+const cors = require("cors");
 const app = express();
 
-app.use(bodyparser.urlencoded({ extended: true }));
+const whitelist = ["http://localhost:5173", "http://example2.com"];
+
+app.options("*", cors());
+
+const corsOptions = {
+  credentials: true,
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyparser.json());
+
+app.get("/", (res) => {
+  res.send("From server");
+});
 
 app.post("/", (req, res) => {
-  const height = req.body.height;
-  const radius = req.body.radius;
-  const segments = req.body.segments;
+  const { height, radius, segments } = req.body;
+
+  const positions = [];
+
+  for (let i = 0; i < segments; i++) {
+    const position = [
+      radius * Math.cos((2 * Math.PI * i) / segments),
+      radius * Math.sin((2 * Math.PI * i) / segments),
+      0,
+      height,
+      height,
+      0,
+      radius * Math.cos((2 * Math.PI * (i + 1)) / segments),
+      radius * Math.sin((2 * Math.PI * (i + 1)) / segments),
+      0,
+    ];
+    positions.push(position);
+  }
+
+  res.json({ positions: positions });
 });
 
 app.listen(PORT, () => {
